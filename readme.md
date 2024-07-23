5b5175562d3b358b8b00b17f7fac91a0c4bf55d9
Ash Weeks <milcktoast@icloud.com>
2024-07-23 14:27:18 -0700

    Updates

---
+++ readme.md
@@ -0,0 +1,7 @@
+# git-templategen
+
+> Simple template generator using git
+
+```
+git-templategen -n kewl_project -b master user/neat_template
+```
--- setup.sh
+++ setup.sh
@@ -1,15 +1,41 @@
 #!/usr/bin/env bash
 
-NAME="$1"
-PROJECT_NAME="$2"
-REPO="git@github.com:jpweeks/${NAME}-boilerplate.git"
+# Usage
+# git-templategen -n kewl_project -b master user/neat_template
+
+REPO_PATH="${@: -1}"
+
+while getopts b:n: flag
+do
+	case "${flag}" in
+		b) BRANCH=${OPTARG};;
+		n) NAME=${OPTARG};;
+	esac
+done
+
+if [[ -z "$REPO_PATH" ]]; then
+	echo "Please specify a repo path"
+	exit 0
+fi
+
+if [[ -z "$NAME" ]]; then
+	echo "Please specify a project name [-n]"
+	exit 0
+fi
+
+REPO="git@github.com:${REPO_PATH}.git"
+
+if [[ -z "$BRANCH" ]]; then
+	BRANCH="master"
+fi
 
 echo "Cloning $REPO ..."
-git clone $REPO $PROJECT_NAME
-cd $PROJECT_NAME
+git clone $REPO $NAME
+cd $NAME
+git checkout $BRANCH
 
-echo "Setting up ..."
-find ./ -type f -exec sed -i '' -e "s/__project-name__/$PROJECT_NAME/g" {} \;
+echo "Setting up $NAME ..."
+find ./ -type f -exec sed -i '' -e "s/__project-name__/$NAME/g" {} \;
 
 echo "Initializing git ..."
 rm -rf .git
